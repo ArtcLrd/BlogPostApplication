f7a3869f1fca13e6be32d26add429a44cfb3702d
ArtcLrd <articlordsmurf@gmail.com>
2025-05-20 16:23:49 +0530

    changed delete fetch

--- frontend/app/posts/[id]/edit/page.tsx
+++ frontend/app/posts/[id]/edit/page.tsx
@@ -25,7 +25,6 @@ const EditBlogPage = () => {
 
       const token = localStorage.getItem('token');
       //fetch delete
-      
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/blogs/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
